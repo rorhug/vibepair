f93a3b2f57cae88b7ef020f82074174e0bdbd1b8
Rory Hughes <544628+rorhug@users.noreply.github.com>
2025-05-10 14:22:01 +0100

    update website

---
+++ app/components/tutorial/landing-page-info.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { TutorialStep } from "./tutorial-step";
+import { ArrowUpRight } from "lucide-react";
+
+export default function LandingPageInfo() {
+  return (
+    <div>
+      <h1>Find People to help you</h1>
+    </div>
+  );
+}
